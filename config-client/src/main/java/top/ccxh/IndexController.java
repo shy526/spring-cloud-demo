383e8c54b667b15d44b47795dee2b3b52350cba8
sunjiaqing <sundada214@outlook.com>
2018-04-24 18:05:15 +0800

    config-service

---
+++ config-client/src/main/java/top/ccxh/IndexController.java
@@ -0,0 +1,15 @@
+package top.ccxh;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @Value("${test}")
+    private String test;
+    @GetMapping("/")
+    public String index(){
+        return test;
+    }
+}
---
+++ config-client/src/main/java/top/ccxh/PutRunner.java
@@ -0,0 +1,17 @@
+package top.ccxh;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PutRunner implements ApplicationRunner {
+    @Value("${test}")
+    private String test;
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        System.out.printf("加载的文件:{%s}\n",test);
+
+    }
+}
