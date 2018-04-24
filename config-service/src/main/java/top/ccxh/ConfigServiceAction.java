9181400f864c4ad176e2c8ee65832cae4fad05de
sunjiaqing <sundada214@outlook.com>
2018-04-24 10:17:43 +0800

    config-service

---
+++ config-service/src/main/java/top/ccxh/ConfigServiceAction.java
@@ -0,0 +1,17 @@
+package top.ccxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceAction {
+
+
+        public static void main(String[] args) {
+            SpringApplication.run(ConfigServiceAction.class, args);
+        }
+
+}
